c50da8a4d7b89f59f1c79f5d337f35ced0695b35
Landon Noss <landon.noss@ff0000.com>
2013-09-06 11:04:24 -0700

    computed cache invalidtion

--- app/src/app.js
+++ app/src/app.js
@@ -2,9 +2,11 @@ var App = window.App = Ember.Application.create();
 
 /* Order and include as you please. */
 require('src/models/*');
+require('src/services/*');
 require('src/controllers/*');
 require('src/views/*');
 require('src/helpers/*');
 require('src/components/*');
 require("src/routes/*");
 require('src/router');
+
--- app/src/services/SubredditService.js
+++ app/src/services/SubredditService.js
@@ -1,4 +1,8 @@
-App.Subreddit = Ember.Object.extend({
+/* everything we define in here is custom -- none of this is 'boilerplate' ember
+we are just defining an object that will be a service layer for creating SingleReddit
+objects and returning them to our controllers when necessary. */
+
+App.SubredditService = Ember.Object.extend({
     baseUrl: 'http://reddit.com/r/',
     postUrl: '/.json',
     name: null,
@@ -21,6 +25,12 @@ App.Subreddit = Ember.Object.extend({
         var self = this;
         // create a promise with a resolve function and a reject function
         return new Ember.RSVP.Promise(function(resolve, reject) {
+
+            // check our cache really fast so we don't slam reddit every time
+            if (self.get('lastFetched') <= 15000) {
+                return self.get('cache');
+            }
+
             // do the ajax thing as usual, but call .then() instead of .done() or .success()
             $.getJSON(self.get('url') + '?jsonp=?').then(function(response) {
                 // create an ember aware array
@@ -30,18 +40,35 @@ App.Subreddit = Ember.Object.extend({
                     // while using the create() method to instantiate the ember object with the properties of the data
                     links.pushObject(App.SingleReddit.create(child.data));
                 });
+
+                // update our cache once we have populated it.
+                self.set('cache', links);
+
                 // finally, resolve the promise.
-                return resolve(links);
+                return resolve(self.get('cache'));
             });
         });
-    }
+    },
+
+    // this is our emberized array where we'll store subreddit content
+    cache: Ember.A(),
+
+    // define a computed property that listens for changes to the cache property above
+    // and resets its timestamp each time cache changes.
+    lastFetched: function () {
+        return new Date().getTime()/1000;
+    }.property('cache')
 });
 
-// test junk, not really used, just an example.
-var gifsReddit = App.Subreddit.create({
+
+/* to test our class above, we'll create
+a gifsReddit object, based on our service. */
+var gifsReddit = App.SubredditService.create({
     name: 'gifs'
 });
 
+
+/* and we'll ask our service to findAll() reddits within it and return it to us as a promise.*/
 gifsReddit.findAll().then(function (items) {
     console.log('reddits!', items);
 });
