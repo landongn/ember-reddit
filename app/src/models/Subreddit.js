abcf19d5511b02b1b67192617406d19f5bde8f98
Landon Noss <landon.noss@ff0000.com>
2013-09-06 10:40:28 -0700

    updated comments

--- app/src/models/Subreddit.js
+++ app/src/models/Subreddit.js
@@ -1,7 +1,7 @@
 App.Subreddit = Ember.Object.extend({
     baseUrl: 'http://reddit.com/r/',
     postUrl: '/.json',
-    name: '',
+    name: null,
     /* computed properties
         here we create a basic computed property based on the above baseUrl and postUrl, as well as the name property.
         we return the computed property as a property called 'url'.
@@ -19,12 +19,18 @@ App.Subreddit = Ember.Object.extend({
     // findall will return a promise for this subreddit, and eventually return an array of reddits for it.
     findAll: function () {
         var self = this;
+        // create a promise with a resolve function and a reject function
         return new Ember.RSVP.Promise(function(resolve, reject) {
+            // do the ajax thing as usual, but call .then() instead of .done() or .success()
             $.getJSON(self.get('url') + '?jsonp=?').then(function(response) {
+                // create an ember aware array
                 var links = Em.A();
                 response.data.children.forEach(function (child) {
+                    // push into the array using the emberized method pushObject
+                    // while using the create() method to instantiate the ember object with the properties of the data
                     links.pushObject(App.SingleReddit.create(child.data));
                 });
+                // finally, resolve the promise.
                 return resolve(links);
             });
         });
