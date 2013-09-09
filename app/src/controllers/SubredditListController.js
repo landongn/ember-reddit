a90cdf230ffe729c10b78b60e35fcfd80c390a98
Landon Noss <landon.noss@ff0000.com>
2013-09-09 15:56:09 -0700

    stufffff

--- app/src/controllers/SubredditListController.js
+++ app/src/controllers/SubredditListController.js
@@ -2,6 +2,10 @@ App.SubredditListController = Em.ArrayController.extend({
     actions: {
         showImage: function (image) {
             console.log("image for the thing", image);
+        },
+        showFullReddit: function (details) {
+            console.log("deatils: ", details);
+            this.transitionToRoute('subreddit.single', details);
         }
     }
 });
--- app/src/router.js
+++ app/src/router.js
@@ -1,7 +1,6 @@
 App.Router.map(function () {
     this.resource('subreddit', {path: '/'}, function () {
-        this.route('list', {path: "/:reddit_id"}, function () {
-            this.route('single', {path: "/:post_id"});
-        });
+        this.route('list', {path: "/:reddit_id"});
+        this.route('single', {path: "/:post_id"});
     });
 });
